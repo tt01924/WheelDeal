87a9819b2bd681ea849863e50d52447f7e64a18a
Tim Farkas <mail@timfarkas.com>
2024-11-01 13:59:21 +0000

    Set up dev environment

---
+++ database.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS WheelDeal;
+
+CREATE DATABASE WheelDeal;
+
+USE WheelDeal;
+
+CREATE TABLE Test (
+    testId VARCHAR(4), 
+    fName VARCHAR(20), 
+    lName VARCHAR(20)
+);
