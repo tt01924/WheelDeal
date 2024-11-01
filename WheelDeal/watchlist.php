370d469169304ef72702900cfd06c1a4da075c80
Tim Farkas <mail@timfarkas.com>
2024-11-01 15:02:27 +0000

    added watchlist.php

---
+++ WheelDeal/watchlist.php
@@ -0,0 +1,22 @@
+<?php include_once("header.php")?>
+<?php require("utilities.php")?>
+
+<div class="container">
+
+<h2 class="my-3">My watchlist</h2>
+
+<?php
+  // This is the My watchlist dummy page. It is intended to display items 
+  // that a buyer has added to their watchlist. This page will be similar 
+  // to browse.php but without a search bar. Once browse.php is integrated 
+  // with a database, this page can be further developed.
+  // Consider moving any reusable functions from browse.php to the shared 
+  // "utilities.php" for use across multiple files.
+  
+  // TODO: Implement user authentication checks (cookie/session).
+  
+  // TODO: Query the database to retrieve auctions the user is watching.
+  
+  // TODO: Iterate over the results and display them as list items.
+  
+?>
