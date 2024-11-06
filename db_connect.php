8fbf32d98f4aaedcf95906b39202164f3514a944
j-simpson01 <prosier.peseta_05@icloud.com>
2024-11-06 17:44:34 +0000

    Changes to db_connect file to get it to work.

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,18 @@
 // connecting to the database using a UNIX socket. Obtain from connection parameters on MAMP webstart.
 
 <?php
-  $db_host = 'localhost';
+  $db_host = '127.0.0.1';
   $db_user = 'root';
   $db_password = 'root';
-  $db_db = 'mydatabase';
- 
-  $mysqli = @new mysqli(
+  $db_db = 'WheelDeal';
+  $db_port = 8889;
+
+  $mysqli = new mysqli(
     $db_host,
     $db_user,
     $db_password,
-    $db_db
+    $db_db,
+	$db_port
   );
 	
   if ($mysqli->connect_error) {
