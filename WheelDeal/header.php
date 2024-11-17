1d72ecd2ab7983ab74e5890a5055f64757a81d89
Peace <peaula.silly@gmail.com>
2024-11-17 16:38:16 +0000

    Reverted changes to header.php

--- WheelDeal/header.php
+++ WheelDeal/header.php
@@ -1,13 +1,18 @@
 <?php
+  // FIXME: At the moment, I've allowed these values to be set manually.
+  // But eventually, with a database, these should be set automatically
+  // ONLY after the user's login credentials have been verified via a 
+  // database query.
   session_start();
 ?>
+
 
 <!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+  
   <!-- Bootstrap and FontAwesome CSS -->
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -18,6 +23,7 @@
   <title>[My Auction Site] <!--CHANGEME!--></title>
 </head>
 
+
 <body>
 
 <!-- Navbars -->
@@ -27,15 +33,12 @@
   </a>
   <ul class="navbar-nav ml-auto">
     <li class="nav-item">
-
+    
 <?php
-  // Displays either login or account info + logout
+  // Displays either login or logout on the right, depending on user's
+  // current status (session).
   if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
-    echo '<span class="navbar-text mr-2">
-            <strong>' . htmlspecialchars($_SESSION['username']) . '</strong>
-            (' . htmlspecialchars($_SESSION['account_type']) . ')
-          </span>
-          <a class="nav-link" href="logout.php">Logout</a>';
+    echo '<a class="nav-link" href="logout.php">Logout</a>';
   }
   else {
     echo '<button type="button" class="btn nav-link" data-toggle="modal" data-target="#loginModal">Login</button>';
@@ -45,37 +48,33 @@
     </li>
   </ul>
 </nav>
-
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <ul class="navbar-nav align-middle">
-    <li class="nav-item mx-1">
+	<li class="nav-item mx-1">
       <a class="nav-link" href="browse.php">Browse</a>
     </li>
-
 <?php
-  // Only show these options if user is logged in
-  if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
-    if ($_SESSION['account_type'] === 'buyer') {
-      echo('
-        <li class="nav-item mx-1">
-          <a class="nav-link" href="mybids.php">My Bids</a>
-        </li>
-        <li class="nav-item mx-1">
-          <a class="nav-link" href="recommendations.php">Recommended</a>
-        </li>
-        <li class="nav-item mx-1">
-          <a class="nav-link" href="watchlist.php">My Watchlist</a>
-        </li>');
-    }
-    if ($_SESSION['account_type'] === 'seller') {
-      echo('
-        <li class="nav-item mx-1">
-          <a class="nav-link" href="mylistings.php">My Listings</a>
-        </li>
-        <li class="nav-item ml-3">
-          <a class="nav-link btn border-light" href="create_auction.php">+ Create auction</a>
-        </li>');
-    }
+  if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == 'buyer') {
+  echo('
+	<li class="nav-item mx-1">
+      <a class="nav-link" href="mybids.php">My Bids</a>
+    </li>
+	<li class="nav-item mx-1">
+      <a class="nav-link" href="recommendations.php">Recommended</a>
+    </li>
+	<li class="nav-item mx-1">
+    <a class="nav-link" href="watchlist.php">My Watchlist</a>
+  </li>');
+  }
+
+  if (isset($_SESSION['account_type']) && $_SESSION['account_type'] == 'seller') {
+  echo('
+	<li class="nav-item mx-1">
+      <a class="nav-link" href="mylistings.php">My Listings</a>
+    </li>
+	<li class="nav-item ml-3">
+      <a class="nav-link btn border-light" href="create_auction.php">+ Create auction</a>
+    </li>');
   }
 ?>
   </ul>
@@ -110,8 +109,3 @@
     </div>
   </div>
 </div> <!-- End modal -->
-
-</body>
-</html>
-
-
